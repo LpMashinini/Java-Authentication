d00bc339c143613b23385b322767ebc4cdf47cfc
LpMashinini <lebogangp84@gmail.com>
2026-03-19 15:15:16 +0200

    added database connection

--- src/main/java/DbConnection.java
+++ src/main/java/DbConnection.java
@@ -1,2 +1,14 @@
+import java.sql.*;
+
 public class DbConnection {
+
+    private static final String url  = "jdbc:mysql://localhost:3306/javadb";
+    private static final String user = "root";
+    private static final String password = "root";
+
+
+    public static Connection getConnection() throws SQLException {
+
+        return DriverManager.getConnection(url,user,password);
+    }
 }
