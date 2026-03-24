410906ff8b8e14474641811fb9b509107c36dbd2
LpMashinini <lebogangp84@gmail.com>
2026-03-24 21:09:45 +0200

    full Authentication System

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,49 +1,114 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
-        String name, email,password;
+        String name, email, password;
 
-        Scanner userInput = new Scanner(System.in);
+        Scanner userOptionInput = new Scanner(System.in);
 
-        System.out.println("Enter name : ");
-        name = userInput.nextLine();
+        System.out.println("Select Option (1 OR 2): ");
+        System.out.println("1) Sign up");
+        System.out.println("2) Login ");
 
-        System.out.println("Enter email : ");
-        email = userInput.nextLine();
+        int userOption = userOptionInput.nextInt();
 
-        System.out.println("Create password: ");
-        password = userInput.nextLine();
+        if (userOption == 1) {
 
-        Hashing hashPass = new Hashing();
-        String hashedPass = hashPass.hashPassword(password);
+            //Sign up option
 
-        try{
+            Hashing hashPass = new Hashing();
 
-            Connection connection = DbConnection.getConnection();
-            String query = "INSERT INTO user(name,email,password) VALUES(?,?,?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            Scanner userInput = new Scanner(System.in);
 
-            preparedStatement.setString(1,name);
-            preparedStatement.setString(2,email);
-            preparedStatement.setString(3,hashedPass);
+            System.out.println("Enter name : ");
+            name = userInput.nextLine();
 
-            int result = preparedStatement.executeUpdate();
+            System.out.println("Enter email : ");
+            email = userInput.nextLine();
 
-            if (result == 1) {
-                System.out.println("Details saved succesfully");
-            } else {
-                System.out.println("Data not captured");
+            System.out.println("Create password: ");
+            password = userInput.nextLine();
+
+            String hashedPass = hashPass.hashPassword(password);
+
+            try {
+
+                Connection connection = DbConnection.getConnection();
+                String query = "INSERT INTO user(name,email,password) VALUES(?,?,?)";
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, email);
+                preparedStatement.setString(3, hashedPass);
+
+                int result = preparedStatement.executeUpdate();
+
+                if (result == 1) {
+                    System.out.println("Details saved succesfully");
+                } else {
+                    System.out.println("Data not captured");
+                }
+
+                preparedStatement.close();
+                connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
-            preparedStatement.close();
-            connection.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } else if (userOption == 2) {
+
+            //Login option
+
+            try {
+
+                Validation verifyPass = new Validation();
+
+                String auth_Email, auth_password;
+
+                Scanner userAuthInput = new Scanner(System.in);
+
+                System.out.println("Enter email : ");
+                auth_Email = userAuthInput.nextLine();
+
+                System.out.println("Enter password: ");
+                auth_password = userAuthInput.nextLine();
+
+                Connection connection = DbConnection.getConnection();
+                String Query = "SELECT email,password FROM user";
+
+                Statement statement = connection.createStatement();
+
+                ResultSet resultSet = statement.executeQuery(Query);
+
+
+                while (resultSet.next()) {
+
+                    String Db_email = resultSet.getString("email");
+                    String Db_password = resultSet.getString("password");
+
+                    boolean validPass = verifyPass.verifyPassword(auth_password, Db_password);
+
+                    if (validPass && auth_Email.equals(Db_email)) {
+                        System.out.println("Login successfully");
+                    } else {
+                        System.out.println("Incorrect email or password");
+                    }
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            //incorrect value input
+            System.out.println("Enter correct value");
         }
+
 
     }
 }
